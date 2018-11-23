5d68e2bf5e24566536c3b2c8cc98732e7c7c44d7
nn6a <869tjmw@gmail.com>
2018-11-23 23:49:15 +0900

    詳細ページから募集要項を取得

--- index.js
+++ index.js
@@ -8,9 +8,25 @@ const puppeteer = require('puppeteer');
     ]
 });
 const page = await browser.newPage();
-await page.goto('https://example.com');
+const navigationPromise = page.waitForNavigation();
 
-await page.screenshot({path: 'screenShotPage.png'});
+await page.goto('https://employment.en-japan.com/desc_894920/?arearoute=1&aroute=0&caroute=0101');
+
+await navigationPromise;
+
+const tableData = await page.evaluate(() => {
+    const data = {};
+    const nodeList = document.querySelectorAll(".descArticleUnit.dataWork table.dataTable tbody tr");
+    nodeList.forEach(_node => {
+        const text = _node.innerText;
+        const separatedText = text.split('\t');
+        const key = separatedText[0];
+        data[key] = separatedText[1];
+    });
+
+    return data
+});
+console.log(tableData);
 
 browser.close();
 })();
