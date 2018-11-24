24f55f1f1d73a9e29aa4a50930e989968c2cd6b1
nn6a <869tjmw@gmail.com>
2018-11-24 14:02:36 +0900

    複数ページを連続して取得

--- index.js
+++ index.js
@@ -8,33 +8,42 @@ const puppeteer = require('puppeteer');
     ]
 });
 const page = await browser.newPage();
-const newPagePromise = new Promise(resolve => browser.once('targetcreated', target => resolve(target.page())));
 
 await page.goto('https://employment.en-japan.com/search/search_list/?occupation=101000_101500_102000_102500_103000_103500_104000_104500_105000_105500_109000&pagenum=1&aroute=0&arearoute=1&caroute=0101');
 
-const toDescButtonSelector = '.list:nth-child(1) > .jobSearchListUnit > .unitBase > .buttonArea > .toDesc';
-await page.waitForSelector(toDescButtonSelector);
-const button = await page.$(toDescButtonSelector);
-await button.click();
+const TO_DESC_BUTTON_SELECTOR = '.list:nth-child(INDEX) > .jobSearchListUnit > .unitBase > .buttonArea > .toDesc';
+const JOB_DESC_PER_PAGE = 50;
+for (let i = 1; i <= JOB_DESC_PER_PAGE; i++) {
+    let toDescButtonSelector = TO_DESC_BUTTON_SELECTOR.replace('INDEX', i);
+    await page.waitForSelector(toDescButtonSelector);
+    let button = await page.$(toDescButtonSelector);
+    await button.click();
 
-let newPage = await newPagePromise;
+    let newPage = await new Promise(resolve => browser.once('targetcreated', target => resolve(target.page())));
 
-const dataTableRowSelector = '.contents > .dataTable > tbody > tr';
-await newPage.waitForSelector(dataTableRowSelector);
-const tableData = await newPage.evaluate((selector) => {
-    const data = {};
-    const nodeList = document.querySelectorAll(selector);
-    nodeList.forEach(_node => {
-        const text = _node.innerText;
-        const separatedText = text.split('\t');
-        const key = separatedText[0];
-        data[key] = separatedText[1];
-    });
+    const dataTableRowSelector = '.contents > .dataTable > tbody > tr';
+    await newPage.waitForSelector(dataTableRowSelector);
+    let tableData = await newPage.evaluate((selector) => {
+        const data = {};
+        const nodeList = document.querySelectorAll(selector);
+        nodeList.forEach(_node => {
+            const text = _node.innerText;
+            const separatedText = text.split('\t');
+            const key = separatedText[0];
+            data[key] = separatedText[1];
+        });
 
-    return data
-}, dataTableRowSelector);
-console.log(tableData);
+        return data
+    }, dataTableRowSelector);
+    console.log(`
+    =======================================================================================
+    == RESULT ${i}
+    =======================================================================================
+    `, tableData);
 
-await newPage.close();
+    await newPage.close();
+    await page.waitFor(10000);
+}
+
 browser.close();
 })();
